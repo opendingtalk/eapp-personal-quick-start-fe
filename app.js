e886da86c48fbf4c81baedfa5118f16d5e42e9d1
opendingtalk <31611459+opendingtalk@users.noreply.github.com>
2018-09-04 22:20:47 +0800

    Update app.js
    删除 corpid，个人应用不需要

--- app.js
+++ app.js
@@ -3,15 +3,11 @@ App({
     console.log('App Launch', options);
     console.log('getSystemInfoSync', dd.getSystemInfoSync());
     console.log('SDKVersion', dd.SDKVersion);
-    this.globalData.corpId = options.query.corpId;
   },
   onShow() {
     console.log('App Show');
   },
   onHide() {
     console.log('App Hide');
-  },
-  globalData: {
-    corpId:''
   }
 });
